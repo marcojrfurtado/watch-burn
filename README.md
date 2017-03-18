f12dcd4dbd42f1607a94badd0fd1a8f6a1b97c75
Antonio Carlos Furtado <salzvede@ualberta.ca>
2017-03-17 21:16:50 -0400

    Remove config file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Watch the World Burn!
+
+Simple web application that displays a daily update of fire alerts across the globe.
+
+
+## Building
+bash build.sh
+
+# Running
+
--- config-example.py
+++ config-example.py
@@ -5,8 +5,7 @@
 import ee
 
 # The service account email address authorized by your Google contact.
-# Set up a service account as described in the README.
-EE_ACCOUNT =  '911396153589-compute@developer.gserviceaccount.com'
+EE_ACCOUNT =  'XXXXXXXXXXXXXXXXXX@developer.gserviceaccount.com'
 
 # The private key associated with your service account in Privacy Enhanced
 # Email format (.pem suffix).  To convert a private key from the RSA format
